db24c114d4b49e8b0970c73a005b448129d10c70
Vadim-Kostetskyi <rasty2308@gmail.com>
2025-08-27 17:54:13 +0300

    creating a project structure

---
+++ src/modules/core/components/MainLayout/index.tsx
@@ -0,0 +1,20 @@
+import { FC, ReactNode } from "react";
+// import Header from "modules/core/containers/Header";
+// import Footer from "modules/core/containers/Footer";
+// import MenuBar from "../MenuBar";
+import styles from "./index.module.scss";
+
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+const MainLayout: FC<MainLayoutProps> = ({ children }) => (
+  <>
+    {/* <MenuBar /> */}
+    {/* <Header /> */}
+    <main className={styles.main}>{children}</main>
+    {/* <Footer /> */}
+  </>
+);
+
+export default MainLayout;
---
+++ src/pages/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
+import 'styles/index.css'
+
+const App = () => (
+  <>
+    {/* <ScrollToTop /> */}
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      {/* <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <ProductFormPage />
+          </PrivateRoute>
+        }
+      /> */}
+    </Routes>
+  </>
+);
+
+export default App
