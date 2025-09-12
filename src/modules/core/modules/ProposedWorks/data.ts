66b0ad9a8e71fd76a548d18126996336ca00413f
Vadim-Kostetskyi <rasty2308@gmail.com>
2025-09-12 11:54:20 +0300

    fix

--- src/modules/core/modules/ProposedWorks/data.ts
+++ src/modules/core/modules/ProposedWorks/data.ts
@@ -1,7 +1,7 @@
-import icon from 'assets/works/icoon_aanbouw_nieuwbouw-300x300.png';
-import icon2 from 'assets/works/icoon_duimstok_renovatie-300x300.png';
-import icon3 from 'assets/works/icoon_gereedschap-hamer_onderhoud-300x300.png';
-import icon4 from 'assets/works/icoon_kerkje_grachtenpand_restauratie-300x300.png';
+// import icon from 'assets/works/icoon_aanbouw_nieuwbouw-300x300.png';
+// import icon2 from 'assets/works/icoon_duimstok_renovatie-300x300.png';
+// import icon3 from 'assets/works/icoon_gereedschap-hamer_onderhoud-300x300.png';
+// import icon4 from 'assets/works/icoon_kerkje_grachtenpand_restauratie-300x300.png';
 
 import icon5 from 'assets/works/renovation.jpg';
 import icon6 from 'assets/works/maintenance.jpg';
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from './HomePage';
-import ProductForm from './ProductFormPage';
+// import ProductForm from './ProductFormPage';
 import 'styles/index.scss';
 import ProjectTable from './ProductTable';
 
