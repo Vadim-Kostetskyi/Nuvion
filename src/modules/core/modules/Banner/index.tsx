583faed59616e738c5d56ef905275ff4d7ddc387
Vadim-Kostetskyi <rasty2308@gmail.com>
2025-09-27 18:21:58 +0300

    Banner mobile stiles

--- src/modules/core/modules/Banner/index.tsx
+++ src/modules/core/modules/Banner/index.tsx
@@ -1,18 +1,5 @@
-// import { useTranslation } from 'react-i18next';
 import styles from './index.module.scss';
 
-const Banner = () => {
-  // const { t } = useTranslation();
-
-  return (
-    <section className={styles.banner}>
-      {/* <div>
-        <p>{t('banner.constructionRenovation')}</p>
-        <h1>{t('banner.slogan')}</h1>
-        <p>{t('banner.quality')}</p>
-      </div> */}
-    </section>
-  );
-};
+const Banner = () => <section className={styles.banner}></section>;
 
 export default Banner;
